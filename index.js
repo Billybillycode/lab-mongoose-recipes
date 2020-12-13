a12eb1726cdf57b7f385a0eb056b0850340a6b78
Gwerborc'h Mahéo <gwerborch.maheo.com>
2020-12-13 20:52:07 +0100

    done

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // ---Iteration 2 ----
     Recipe.create({
       title: "Pizza 3 fromage",
       level: "Amateur Chef",
       cuisine: "Italian",
       dishType: "main_course",
-      ingredient: ['mozzarella', 'comté', 'gorgonzola'],
-      image:
-          "https://assets.afcdn.com/recipe/20160926/5624_w600.jpg",
+      ingredient: ["mozzarella", "comté", "gorgonzola"],
+      image: "https://assets.afcdn.com/recipe/20160926/5624_w600.jpg",
       duration: 20,
       creator: "Mama Primi",
     })
     .then((recipe) => {
-    console.log(recipe.title);
+      console.log(recipe.title);
+      // ---Iteration 3---
 
-    Recipe.insertMany(data);
-    data.forEach((element) => {
-      console.log(element.title)
-    })
+      Recipe.insertMany(data);
+      data.forEach((element) => {
+        console.log(element.title);
+      });
+    });
+
+    //--Iteration 4 ---
+    Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
+    console.log("!!!succes duration changed!!!");
+
+    Recipe.deleteOne({ title: "Carrot Cake" });
   })
 
-  Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese" },
-  )
-})
-
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  // TODO: write the schema --- Iteration 1 ------
   title: {
     type: String,
     unique: true
